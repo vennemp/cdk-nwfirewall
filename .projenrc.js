cf1e5328bdca140554f386a52ca9d75689c7c3d2
Matthew Venne <mvenen@stackarmor.com>
2021-12-30 22:41:36 -0500

    a

--- .projenrc.js
+++ .projenrc.js
@@ -6,7 +6,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   docgen: true,
   cdkVersionPinning: false,
   releaseToNpm: true,
-  devDeps: ['@aws-cdk/aws-iam', '@aws-cdk/aws-ec2', '@aws-cdk/aws-lambda', '@aws-cdk/aws-logs', '@aws-cdk/aws-networkfirewall', '@aws-cdk/core'],
+  devDeps: ['jsii-pacmak', '@aws-cdk/aws-iam', '@aws-cdk/aws-ec2', '@aws-cdk/aws-lambda', '@aws-cdk/aws-logs', '@aws-cdk/aws-networkfirewall', '@aws-cdk/core'],
   keywords: ['aws', 'awscdk', 'aws-cdk', 'cdk', 'firewall', 'network-automation', 'network-security', 'netsec'],
   defaultReleaseBranch: 'main',
   deps: ['@aws-cdk/aws-iam', '@aws-cdk/aws-ec2', '@aws-cdk/aws-lambda', '@aws-cdk/aws-logs', '@aws-cdk/aws-networkfirewall', '@aws-cdk/core'],
