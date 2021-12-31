d70f1063690023a16efda8a0aedbec8476682e14
Matthew Venne <mvenen@stackarmor.com>
2021-12-30 20:14:22 -0500

    debug

--- .projenrc.js
+++ .projenrc.js
@@ -1,5 +1,4 @@
 const { awscdk } = require('projen');
-const { AwsCdkDeps } = require('projen/lib/awscdk');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Matthew Venne',
   authorAddress: 'vennemp@gmail.com',
@@ -7,13 +6,11 @@ const project = new awscdk.AwsCdkConstructLibrary({
   docgen: true,
   releaseToNpm: true,
   keywords: ['aws', 'awscdk', 'aws-cdk', 'cdk', 'firewall', 'network-automation', 'network-security'],
-  AwsCdkDeps: ['@aws-cdk/core', '@aws-cdk/aws-ec2', '@aws-cdk/aws-iam', '@aws-cdk/aws-lambda', '@aws-cdk/aws-logs', '@aws-cdk/aws-networkfirewall'],
   defaultReleaseBranch: 'main',
   deps: ['@aws-cdk/aws-iam', '@aws-cdk/aws-ec2', '@aws-cdk/aws-lambda', '@aws-cdk/aws-logs', '@aws-cdk/aws-networkfirewall', '@aws-cdk/core'],
   peerDeps: ['@aws-cdk/aws-iam', '@aws-cdk/aws-ec2', '@aws-cdk/aws-lambda', '@aws-cdk/aws-logs', '@aws-cdk/aws-networkfirewall', '@aws-cdk/core'],
   name: 'cdk-nwfirewall',
   repositoryUrl: 'https://github.com/vennemp/cdk-nwfirewall.git',
-
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
